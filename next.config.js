07095d955de44a398e05078dccb8ec47ae1f1b77
Elizabeth Stein <purplegumdropz@gmail.com>
2026-04-16 07:32:55 -0400

    chore: add renovate, bundle-analyzer, deps

--- next.config.js
+++ next.config.js
@@ -1,3 +1,6 @@
+import withBundleAnalyzerInit from "@next/bundle-analyzer";
+const withBundleAnalyzer = withBundleAnalyzerInit({ enabled: process.env.ANALYZE === "true" });
+
 /** @type {import('next').NextConfig} */
 const nextConfig = {};
 
